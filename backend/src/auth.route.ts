70ddf0fcf54d1ba24c7e5973f909ad5bfaaf1f56
EMSpy <emilspiritu@gmail.com>
2026-01-06 11:10:18 -0500

    feat: Register user - auth middleware - login endpoint - fix register endpoint with username validation

--- backend/src/auth.route.ts
+++ backend/src/auth.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv"
 import { db } from "./config/db.js";
 
+dotenv.config()
 const router = Router()
 
 
@@ -11,9 +13,9 @@ router.post("/register", async (req, res) => {
     try {
         const { userName, email, password } = req.body
 
-        const checkUser = await db.query("SELECT email FROM users WHERE email = $1", [email])
+        const checkUser = await db.query("SELECT email, username FROM users WHERE email = $1 OR username = $2", [email, userName])
           
-        if (checkUser.rows.length > 0) return res.status(409).json({ message: "User already exists" })
+        if (checkUser.rows.length > 0 ) return res.status(409).json({ message: "User already exists" })
             
         const hashedPassword = await bcrypt.hash(password, 10)
 
@@ -29,6 +31,37 @@ router.post("/register", async (req, res) => {
     }
 })
 
+
+router.post("/login" , async (req, res)=> {
+    try {
+        const { email, password } = req.body
+
+        const result = await  db.query("SELECT * FROM users WHERE email = $1", [email])
+
+        if(result.rows.length === 0) return res.status(401).json({message: "Invalid credentials"})
+
+        const user =  result.rows[0]
+
+        const valid = await bcrypt.compare(password, user.password)
+
+        if(!valid) return res.status(401).json({message: "Invalid credentials"})
+
+        const token = jwt.sign(
+            {id: user.id , username: user.username},
+            process.env.JWT_SECRET!,
+            {expiresIn: "1d"}
+        )
+
+        res.json({
+            token,
+            username: user.username
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: "Internar server error"})
+    }
+
+})
 
 
 
---
+++ backend/src/middleware/authMiddleware.ts
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export function authMiddleware(req: Request, res: Response, next:NextFunction) {
+  const auth = req.headers.authorization;
+
+  if (!auth) return res.sendStatus(401);
+
+  const token = auth.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    (req as any).user = decoded;
+    next();
+  } catch {
+    res.sendStatus(401);
+  }
+}
